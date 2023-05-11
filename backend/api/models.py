4770c1da3366f857443b46256a3d3419997139b2
Arian <dervishaj@iMacdeaistrator.orgex.po>
2023-05-11 16:19:26 +0200

    ajout des models

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,51 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+class Question(models.Model):
+    text = models.TextField()
+
+    def __str__(self):
+        return self.text
+
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    text = models.CharField(max_length=255)
+    is_correct = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.text
+
+
+class Player(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    score = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.user.username
+
+
+class QuestionReport(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    player = models.ForeignKey(Player, on_delete=models.CASCADE)
+    description = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Report by {self.player.user.username} - Question: {self.question}"
+
+
+class HighScore(models.Model):
+    player = models.ForeignKey(Player, on_delete=models.CASCADE)
+    score = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.player.user.username} - Quiz: {self.quiz.title}"
