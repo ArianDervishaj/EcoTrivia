4e2c934eae6a714372a33ed6534994b4439f8e6c
ArianDervishaj <ArianDervishaj@icloud.com>
2023-05-11 20:07:24 +0200

    admin settings + api's urls

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
+from api.models import Category, Choice, HighScore, Player, Question, QuestionReport
+
 # Register your models here.
+admin.site.register(Category)
+admin.site.register(Question)
+admin.site.register(Choice)
+admin.site.register(Player)
+admin.site.register(QuestionReport)
+admin.site.register(HighScore)
